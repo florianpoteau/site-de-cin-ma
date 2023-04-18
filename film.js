6a158c2a2b1068f82b8db62e99db43c4fa8954fa
Poteau Florian <florianpoteau59@gmail.com>
2023-04-18 15:35:43 +0200

    test

--- film.js
+++ film.js
@@ -1,14 +1,16 @@
+// Constante film
+
 const film1 = document.querySelector(".film1");
 const film2 = document.querySelector(".film2");
 const film3 = document.querySelector(".film3");
-
 const film6 = document.querySelector(".film6");
 const film7 = document.querySelector(".film7");
 const film8 = document.querySelector(".film8");
-
 const film11 = document.querySelector(".film11");
 const film12 = document.querySelector(".film12");
 const film13 = document.querySelector(".film13");
+
+// Constante texte des films
 
 const texte1 = document.querySelector(".texte1");
 const texte2 = document.querySelector(".texte2");
@@ -20,14 +22,9 @@ const texte7 = document.querySelector(".texte7");
 const texte8 = document.querySelector(".texte8");
 const texte9 = document.querySelector(".texte9");
 
-const texteb = document.querySelector(".texteb")
-
-console.log(texteb);
-
 function film (film, texte){
     film.addEventListener("click", () =>{
         texte.style.visibility = "visible";
-        texteb.style.border = "solid 1px"
     })
 }
 
@@ -41,5 +38,28 @@ film(film11, texte7)
 film(film12, texte8)
 film(film13, texte9)
 
-// film(film2, film01)
-// film(film1, film00)
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
+
+function bouton (bouton, texte){
+    bouton.addEventListener("click", () =>{
+        texte.style.visibility = "visible";
+    })
+}
+
+bouton(btn1, texte1);
+bouton(btn2, texte2);
+bouton(btn3, texte3);
+bouton(btn4, texte4);
+bouton(btn5, texte5);
+bouton(btn6, texte6);
+bouton(btn7, texte7);
+bouton(btn8, texte8);
+bouton(btn9, texte9);
