0ce9050ce71e75507e5c3489f1e959e459140ce9
Poteau Florian <florianpoteau59@gmail.com>
2023-04-19 12:01:52 +0200

    creation page info

--- film.js
+++ film.js
@@ -22,6 +22,7 @@ const texte7 = document.querySelector(".texte7");
 const texte8 = document.querySelector(".texte8");
 const texte9 = document.querySelector(".texte9");
 
+
 function film (film, texte){
     film.addEventListener("click", () =>{
         texte.style.visibility = "visible";
@@ -38,6 +39,8 @@ film(film11, texte7)
 film(film12, texte8)
 film(film13, texte9)
 
+// Constante bouton savoir plus
+
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
@@ -48,18 +51,32 @@ const btn7 = document.querySelector(".btn7");
 const btn8 = document.querySelector(".btn8");
 const btn9 = document.querySelector(".btn9");
 
-function bouton (bouton, texte){
+// Constante btn voir
+
+const btn11 = document.querySelector(".btn11");
+const btn22 = document.querySelector(".btn22");
+const btn33 = document.querySelector(".btn33");
+const btn44 = document.querySelector(".btn44");
+const btn55 = document.querySelector(".btn55");
+const btn66 = document.querySelector(".btn66");
+const btn77 = document.querySelector(".btn77");
+const btn88 = document.querySelector(".btn88");
+const btn99 = document.querySelector(".btn99");
+
+
+function bouton (bouton, texte, btn){
     bouton.addEventListener("click", () =>{
         texte.style.visibility = "visible";
+        btn.style.display = "block";
     })
 }
 
-bouton(btn1, texte1);
-bouton(btn2, texte2);
-bouton(btn3, texte3);
-bouton(btn4, texte4);
-bouton(btn5, texte5);
-bouton(btn6, texte6);
-bouton(btn7, texte7);
-bouton(btn8, texte8);
-bouton(btn9, texte9);
+bouton(btn1, texte1, btn11);
+bouton(btn2, texte2, btn22);
+bouton(btn3, texte3, btn33);
+bouton(btn4, texte4, btn44);
+bouton(btn5, texte5, btn55);
+bouton(btn6, texte6, btn66);
+bouton(btn7, texte7, btn77);
+bouton(btn8, texte8, btn88);
+bouton(btn9, texte9, btn99);
